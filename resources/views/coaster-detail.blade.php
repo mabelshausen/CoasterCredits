e7e259c998547d62c88792592ed8491d895acd8c
marijn.abelshausen <marijn.abelshausen@student.howest.be>
2021-12-15 16:05:16 +0100

    Add marker to map

--- resources/views/coaster-detail.blade.php
+++ resources/views/coaster-detail.blade.php
@@ -60,6 +60,7 @@
         <div id="single-coaster-map" class="map"></div>
         <script>
             let park = @json($park);
+            let coordinates = [park['longitude'], park['latitude']];
 
             const map = new ol.Map({
                 target: 'single-coaster-map',
@@ -69,10 +70,33 @@
                     })
                 ],
                 view: new ol.View({
-                    center: ol.proj.fromLonLat([park['longitude'], park['latitude']]),
+                    center: ol.proj.fromLonLat(coordinates),
                     zoom: 10
                 })
             });
+
+            const marker = new ol.Feature({
+                type: 'marker',
+                geometry: new ol.geom.Point(ol.proj.fromLonLat(coordinates))
+            });
+
+            const markerVectors = new ol.source.Vector({
+                features: [marker]
+            });
+
+            const markerStyle = new ol.style.Style({
+                image: new ol.style.Icon({
+                    src: "../images/marker.png",
+                    anchor: [0.5, 1]
+                })
+            });
+
+            const markerLayer = new ol.layer.Vector({
+                source: markerVectors,
+                style: markerStyle
+            });
+
+            map.addLayer(markerLayer);
         </script>
     </div>
 </x-app-layout>
